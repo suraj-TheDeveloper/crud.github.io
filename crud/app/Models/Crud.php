f375a4106ecab057e9b1b5a7948b8fbb5a8ee049
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2022-01-05 15:38:35 +0530

    Model file

---
+++ crud/app/Models/Crud.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Crud extends Model
+{
+    use HasFactory;
+
+    protected $table = 'crud';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'status',
+        'sdate',
+        'edate',
+    ];
+}
