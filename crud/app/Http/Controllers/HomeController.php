0b2038cf938352ce885d8c14a6941df924c51c60
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2022-01-05 15:37:57 +0530

    Controller file

--- crud/app/Http/Controllers/HomeController.php
+++ crud/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Crud;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,56 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user_id = Auth::user()->id;
+        $showdata = Crud::where("user_id", $user_id)->get();
+        return view('home', compact('showdata'));
     }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validates = $request->validate([
+            'name' => ['required'],
+            'description' => ['required'],
+            'sdate' => ['required'],
+            'edate' => ['required']
+        ]);
+        $user_id = Auth::user()->id;
+        $status = 0;
+        $task = Crud::create([
+            'user_id' => $user_id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'status' => $status,
+            'sdate' => $request->sdate,
+            'edate' => $request->edate,
+        ]);
+        return redirect(route('home'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //show all task
+        $showtask = Crud::where("id", $id)->get();
+        return view('show', compact('showtask'));
+        // return view('show');
+    }
+
 }
